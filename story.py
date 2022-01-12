6a4e686124444a24966259324341d0c8c5f4a159
Maddox Nichols <maddox0816@gmail.com>
2022-01-12 11:34:55 -0500

    Update story.py

--- story.py
+++ story.py
@@ -7,3 +7,4 @@ for i in range(karinna): #Jedo
   print(f"Your randomly selected number is {number}") #Drew
 person_name=input("Enter your name: ") #Maddox
 def stealIdentity(person_name): #Drew
+   import requests #Maddox
