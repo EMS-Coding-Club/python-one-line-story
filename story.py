f544db4e99f50c78db0ed5df9dee9a8215a7b39a
Karinna2 <97608075+Karinna2@users.noreply.github.com>
2022-01-12 08:51:01 -0500

    Update story.py

--- story.py
+++ story.py
@@ -3,3 +3,4 @@ karinna=input("On a scale of 1-10, what's your favorite color? ") #Karinna
 if karinna > 5: #maddox
   print("that is a numeber") #Sophie
 for i in range(karinna): #Jedo
+  number=random.randint(1,100) #karinna
