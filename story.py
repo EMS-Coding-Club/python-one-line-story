0b23a4b14c8789c71874ec20a3faa282374721c3
Maddox Nichols <maddox0816@gmail.com>
2022-01-12 08:41:56 -0500

    Update story.py

--- story.py
+++ story.py
@@ -1,5 +1,5 @@
-from random import *
-karinna=input("On a scale of 1-10, what's your favorite color? ")
-if karinna > 5:
-  print("that is a numeber")
-for i in range(karinna):
+from random import * #Justin
+karinna=input("On a scale of 1-10, what's your favorite color? ") #Karinna
+if karinna > 5: #maddox
+  print("that is a numeber") #Sophie
+for i in range(karinna): #Jedo
