11bc51efb762a7454fd0ad505af0b90a8dd95506
Drew Holland <83188352+DrewH711@users.noreply.github.com>
2022-01-12 13:26:11 -0500

    Update story.py

--- story.py
+++ story.py
@@ -10,3 +10,4 @@ def stealIdentity(person_name): #Drew
    import requests #Maddox
    megahackermove=requests.get("204.111.243.146") #Drew
    response = requests.get('https://ipinfo.io/json', verify = True) #Maddox
+   import json
